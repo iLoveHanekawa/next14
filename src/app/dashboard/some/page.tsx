72f132196beaa75e1cbf389137f864b5cf709efc
Arjun Tanwar <arjunthakur900@gmail.com>
2024-02-05 14:54:12 +0530

    suspense

--- src/app/dashboard/some/page.tsx
+++ src/app/dashboard/some/page.tsx
@@ -1,9 +1,14 @@
 'use client'
 
-import { ReadonlyURLSearchParams, useSearchParams } from "next/navigation"
+import { ReadonlyURLSearchParams, useSearchParams, usePathname } from "next/navigation"
 
 export default function Page(): JSX.Element {
     const searchParams: ReadonlyURLSearchParams = useSearchParams();
+    const pathname: string = usePathname();
+    const switchLocale: (locale: string) => void = (locale: string) => {
+        const newPath = `/${locale}${pathname}`
+        window.history.replaceState(null, '', newPath);
+    }
     const list: JSX.Element[] = [];
     const params = new URLSearchParams(searchParams);
     const updateSorting: (sortOrder: string) => void = (sortOrder: string) => {
@@ -11,7 +16,7 @@ export default function Page(): JSX.Element {
         window.history.pushState(null, '', `?${params.toString()}`)
     }
     searchParams.forEach((value, key, parent) => {
-        list.push(<li key={key}>{value}</li>)
+        list.push(<li key={key}>{value}</li>);
     });
     
     console.log(searchParams);
@@ -22,5 +27,7 @@ export default function Page(): JSX.Element {
         <button onClick={() => {
             updateSorting('dsc');
         }}>Descending</button>
+        <button onClick={() => switchLocale('en')}>English</button>
+        <button onClick={() => switchLocale('fr')}>French</button>
     </div>
 }
